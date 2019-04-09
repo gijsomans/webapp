17c35e2107fa0e8b5243bcbc90c4872d6da32ec7
gijsomans <gijsomans@gmail.com>
2019-04-09 13:43:58 +0200

    hoi

---
+++ index.js
@@ -0,0 +1,26 @@
+var electron = require('electron');
+var BrowserWindow = electron.BrowserWindow;
+var app = electron.app;
+
+app.on('ready', function () {
+    var appWindow;
+    var infoWindow;
+appWindow = new BrowserWindow({show: false});
+appWindow.loadURL('file://' + __dirname + '/bootstrap.html');
+
+infoWindow = new BrowserWindow({
+    width:400, 
+    height:300,
+    frame: false,
+    show: false
+})
+infoWindow.loadURL('file://' + __dirname + '/info.html');
+
+appWindow.once('ready-to-show', function(){
+    appWindow.show();
+    infoWindow.show();
+    setTimeout(function(){
+        infoWindow.hide();
+    },2000)
+});
+})
---
+++ test.bat
@@ -0,0 +1,1 @@
+C:\Users\gijso\electron-quick-start\node_modules\electron\dist\electron.exe index
