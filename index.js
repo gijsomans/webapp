eb8b53b897863c5664e5983d8d59e616998cdafa
gijsomans <gijsomans@gmail.com>
2019-04-11 11:27:20 +0200

    hoi

--- index.js
+++ index.js
@@ -7,18 +7,17 @@ var app = electron.app;
 app.on('ready', function () {
     var appWindow;
     var infoWindow;
-appWindow = new BrowserWindow({show: false});
+appWindow = new BrowserWindow({
+    show: false,
+    width:3840, 
+    height:2160,
+    frame: false
+    
+});
+
 appWindow.loadURL('file://' + __dirname + '/bootstrap.html');
 
-infoWindow = new BrowserWindow({
-   
-    
-    
-    width:400, 
-    height:300,
-    frame: false,
-    show: false
-})
+
 
 appWindow.once('ready-to-show', function(){
     appWindow.show();
